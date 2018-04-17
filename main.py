7c2cf6557df80298e8ad7f29cdcde500e9b7ebb6
Katarzyna Grzesiak <katarzyna.grzesiak@ericsson.com>
2018-04-17 18:07:53 +0200

    poprawa kolorku

--- main.py
+++ main.py
@@ -57,8 +57,8 @@ cap = cv2.VideoCapture('pilkarzyki.mp4')
 
 # template
 # boundaries = [([G_min, B_min, R_min], [G_max, B_max, R_max])]
-# boundaries = [([25, 146, 190], [62, 174, 250])]
-boundaries_ball = [([25, 120, 190], [100, 255, 255])]
+boundaries = [([0, 0, 190], [62, 174, 250])]
+boundaries_ball = [([0, 50, 150], [50, 180, 200])]
 boundaries_red_players = [([0, 0, 140], [80, 80, 255])]
 # boundaries_blue_players = [([100, 0, 0], [255, 100, 50])]
 
@@ -94,7 +94,7 @@ while cap.isOpened():
 
         # show the images
         cv2.imshow("images", output)
-        cv2.waitKey(0)
+        cv2.waitKey(1)
 
         r_min = boundaries[0][0][2]
         r_max = boundaries[0][1][2]
