5894fb8d3aaf0ec845324ab0c6ad838bf5472daf
Katarzyna Grzesiak <katarzyna.grzesiak@ericsson.com>
2018-04-18 15:18:22 +0200

    wyswietlanie loga

--- main.py
+++ main.py
@@ -58,6 +58,10 @@ def main():
 
     while cap.isOpened():
         ret, frame = cap.read()
+
+        image = cv2.imread("logo.png", cv2.IMREAD_UNCHANGED)
+        transparentOverlay(frame, image, (1750, 920), 0.3)
+
         buffer[index % BUFFER_SIZE] = frame
         index += 1
 
@@ -76,7 +80,7 @@ def main():
                 counter = 0
             else:
                 counter += 1
-                if counter == 50:
+                if counter == 35:
                     print("Saving file")
                     save_buffer_to_file(buffer, index, output_filename, file_counter)
                     file_counter += 1
@@ -175,5 +179,30 @@ def set_colors(boundaries):
     return [([g_min, b_min, r_min], [g_max, b_max, r_max])]
 
 
+def transparentOverlay(src, overlay, pos=(0, 0), scale=1):
+    """
+    :param src: Input Color Background Image
+    :param overlay: transparent Image (BGRA)
+    :param pos:  position where the image to be blit.
+    :param scale : scale factor of transparent image.
+    :return: Resultant Image
+    """
+    overlay = cv2.resize(overlay, (0, 0), fx=scale, fy=scale)
+    h, w, _ = overlay.shape  # Size of foreground
+    rows, cols, _ = src.shape  # Size of background Image
+    y, x = pos[0], pos[1]  # Position of foreground/overlay image
+
+    # loop over all pixels and apply the blending equation
+    for i in range(h):
+        for j in range(w):
+            if x + i >= rows or y + j >= cols:
+                continue
+            alpha = float(overlay[i][j][3] / 255.0)  # read the alpha channel
+            src[x + i][y + j] = alpha * overlay[i][j][:3] + (1 - alpha) * src[x + i][y + j]
+    return src
+
+
 if __name__ == '__main__':
     main()
+
+
